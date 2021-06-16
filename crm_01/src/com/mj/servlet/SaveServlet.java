5a3eae45b4549a108deb561a00c22b4348174c2f
TpTang <83406050+TpTang@users.noreply.github.com>
2021-06-16 09:43:21 +0800

    Update SaveServlet.java

--- crm_01/src/com/mj/servlet/SaveServlet.java
+++ crm_01/src/com/mj/servlet/SaveServlet.java
@@ -10,6 +10,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/*
+ *处理客户端执行"添加"操作发送的数据
+ */
 @WebServlet("/save")
 public class SaveServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -29,11 +32,12 @@ public class SaveServlet extends HttpServlet {
         customer.setAge(Integer.valueOf(age));
         customer.setHeight(Double.valueOf(height));
 
-        //保存数据
+        //模拟保存数据
         Data.add(customer);
 
         //转发
-//        request.getRequestDispatcher("/list").forward(request, response);
+        //request.getRequestDispatcher("/list").forward(request, response);
+        
         //重定向：如果Location不是完整URL->浏览器拿到IP+端口拼接Location去请求资源
         response.sendRedirect("/hello/list");
     }
